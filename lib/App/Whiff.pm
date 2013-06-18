e2189be828e6608f7f56b203b963a4284be81430
Ricardo Signes <rjbs@cpan.org>
2013-06-18 13:25:02 -0400

    dzilify

--- lib/App/Whiff.pm
+++ lib/App/Whiff.pm
@@ -1,29 +1,16 @@
 use strict;
 use warnings;
 package App::Whiff;
+# ABSTRACT: find the first executable of a series of alternatives
 
-=head1 NAME
-
-App::Whiff - find the first executable of a series of alternatives
-
-=head1 VERSION
-
-version 0.002
-
-=cut
-
-our $VERSION = '0.002';
-
-use File::Which;
+use File::Which ();
 
 =head1 DESCRIPTION
 
 This module implements logic used by the F<whiff> command, which takes a number
 of command names and returns the first one that exists and is executable.
 
-=head1 METHODS
-
-=head2 find_first
+=method find_first
 
   my $filename = App::Whiff->find_first(\@names);
 
@@ -45,7 +32,7 @@ sub find_first {
   return;
 }
 
-=head2 run
+=method run
 
 This method is called by the F<whiff> program to ... well, run.
 
@@ -59,18 +46,5 @@ sub run {
   die "no alternative found\n" unless $file;
   print "$file\n";
 }
-
-=head1 BUGS
-
-Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>. I will be notified, and then you'll automatically be
-notified of progress on your bug as I make changes.
-
-=head1 COPYRIGHT
-
-Copyright 2008, Ricardo SIGNES.  This program is free software;  you can
-redistribute it and/or modify it under the same terms as Perl itself.
-
-=cut
 
 1;
